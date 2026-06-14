71e58318c1328af24c3382657bc7998204bf24fb
narayon231206 <narayon231206@gmail.com>
2026-06-14 23:08:50 +0600

    feat: add theme context for dark and light mode support

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,66 @@
+'use client';
+
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  // Load saved theme
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme) {
+      setTheme(savedTheme);
+
+      document.documentElement.classList.toggle(
+        'dark',
+        savedTheme === 'dark'
+      );
+    } else {
+      // Default Light Theme
+      setTheme('light');
+
+      document.documentElement.classList.remove('dark');
+
+      localStorage.setItem('theme', 'light');
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+
+    setTheme(newTheme);
+
+    localStorage.setItem('theme', newTheme);
+
+    document.documentElement.classList.toggle(
+      'dark',
+      newTheme === 'dark'
+    );
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+
+  if (!context) {
+    throw new Error(
+      'useTheme must be used within a ThemeProvider'
+    );
+  }
+
+  return context;
+};
